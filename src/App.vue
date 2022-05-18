41faac814d2e8214aac087a7f365a12e8c8de16f
rosicky-ctrl <53788865+rosicky-ctrl@users.noreply.github.com>
2022-05-18 18:24:59 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="wrapper">
+    <page-header>Sklep</page-header>
+    <page-nav>
+      <router-link :to="{ name: 'Home' }">Home</router-link>
+      <router-link :to="{ name: 'Products' }">Produkty</router-link>
+      <router-link :to="{ name: 'About' }">O sklepie</router-link>
+    </page-nav>
+    <page-main>
+      <router-view></router-view>
+    </page-main>
+    <page-aside>Koszyk</page-aside>
+    <page-footer>Sklep 2022</page-footer>
+  </div>
+</template>
+
+<script setup>
+  import PageHeader from './components/PageHeader.vue'
+  import PageFooter from './components/PageFooter.vue'
+  import PageMain from './components/PageMain.vue'
+  import PageNav from './components/PageNav.vue'
+  import PageAside from './components/PageAside.vue'
+</script>
+
+<style>
+  body { 
+      margin: 0; 
+      padding: 0;
+      font-family: 'Segoe UI', Tahoma, Verdana, sans-serif;
+  }
+  .wrapper { 
+    display: grid;
+    grid-template-rows: min-content min-content 1fr min-content;
+    grid-template-columns: 70% 30%;
+    height: calc(100vh - 1rem);
+    padding: .5rem;
+  }
+  header {
+     grid-column: 1/3;
+     padding: 1rem;
+     background-color: blue;
+     color: white
+  }
+  nav a+a {
+    margin-left: 1rem
+  }
+  nav {
+    grid-column: 1/3;
+     background-color: aquamarine;
+     padding: 1rem;
+  }
+  main { 
+    background-color: beige;
+    padding: 1rem;
+  }
+  aside { 
+  background-color: gold;
+  padding: 1rem;
+}
+  footer {
+    grid-column: 1/3;
+    background-color: yellowgreen;
+    padding: 1rem;
+    text-align: center;
+  }
+@media all and (max-width: 800px) {
+    .wrapper { 
+    grid-template-columns: 100%;
+  }
+  .wrapper > * {
+    grid-column: 1/2;
+  }
+}
+</style>
---
+++ src/components/ProductsTable.vue
@@ -0,0 +1,42 @@
+<template>
+    <table class="products">
+        <thead>
+            <tr>
+                <th>Nazwa</th>
+                <th>Cena</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="p of products" :key="p.id">
+                <td>{{ p.name }}</td>
+                <td>{{ p.price }}</td>
+                <td><button>+</button></td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+  import products from '../data.js'
+</script>
+
+<style>
+    table.products {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    table.products td, table.products th {
+        padding: 0.5rem;
+        border-bottom: 1px solid black;
+    }
+    table.products td:nth-child(1) {
+        width: 100%;
+    }
+    table.products td:nth-child(2) {
+        text-align: right;
+    }
+    table.products td:nth-child(3) {
+        text-align: center;
+    }
+</style>
